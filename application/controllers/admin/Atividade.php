c3ccea2f82258b239fbd6a71cedb13f7125b4518
Silverio O Paula <contato@silverio.eti.br>
2019-10-27 18:11:46 -0300

    atividade

--- application/controllers/admin/Atividade.php
+++ application/controllers/admin/Atividade.php
@@ -17,10 +17,24 @@ class Atividade extends CI_Controller {
         }
         define('BASE_URL', base_url());
         define('USUARIO_NOME', getNomeUsuario());
+        $this->load->helper('converte_data');
+        $this->load->model('admin/mod_atividade');
     }
     
     public function index(){
         $this->load->view('admin/atividade');
+    }
+
+    public function listar(){
+        $limit = $this->input->get('limit');
+        $offset = $this->input->get('offset');
+        $sort = $this->input->get('sort');
+        $order = $this->input->get('order');
+        $search = $this->input->get('search');
+        $total = $this->mod_atividade->listar($limit, $offset, $sort, $order, $search, true);
+        $atividades = $this->mod_atividade->listar($limit, $offset, $sort, $order, $search);
+        $data = array('total' => $total, 'atividades' => $atividades);
+        echo json_encode($data);
     }
 
 }
--- assets/app/atividade-admin.js
+++ assets/app/atividade-admin.js
@@ -1,3 +1,75 @@
+var $table = $("#atividades");
+var base_url = "http://localhost:8000/";
+
 $(function(){
-    
+    $table.bootstrapTable({
+        url: base_url + "admin/atividade/listar",
+        queryParamsType: "limit",
+        queryParams : queryParams,
+        sidePagination: "server",
+        showRefresh: false,
+        showColumns: false,
+        showToggle: false,
+        pagination: true,
+        trimOnSearch: false,
+        clickToSelect: true,
+        search: true,
+        sortName: "atividade",
+        sortOrder: "asc",
+        pageList: [10, 25, 50, 100],
+        pageSize: 10,
+        theadClasses: "thead-light",
+        classes: "table table-bordered table-sm table-hover",
+        trimOnSearch: false,
+        toolbar: "#toolbar",
+        detailView: true,
+        detailFormatter: detalhesFormatter,
+        icons: window.icones,
+        columns: [
+            { field: "selecionado", checkbox: true },
+            { field: "data", title: "Data", sortable: true },
+            { field: "aluno_ra", title: "RA", sortable: true },
+            { field: "nome", title: "Aluno", sortable: true },
+            { field: "email", title: "Email", sortable: true }
+        ],
+        responseHandler: function ( data ) {
+            return {
+                total: data.total,
+                rows: data.atividades
+            };
+        },
+        formatLoadingMessage: function () {
+            return "<span style='font-size: 0.85rem;margin: 5px;'>Carregando</span>";
+        }              
+    });      
 });
+
+function detalhesFormatter(index, row){
+    var html = [];
+    html.push("<div class='card-group'>" +
+    "<div class='card'>" +
+      "<div class='card-body'>" +
+        "<h5 class='card-title'>Atividade</h5>" +
+        "<p class='card-text'>" + row.atividade + "</p>" +
+      "</div>" +
+    "</div>" +
+    "<div class='card'>" +
+      "<div class='card-body'>" +
+        "<h5 class='card-title'>Modalidade</h5>" +
+        "<p class='card-text'>" + row.modalidade + "</p>" + 
+      "</div>" +
+    "</div>" +
+      "<div class='card'>" +
+      "<div class='card-body'>" +
+        "<h5 class='card-title'>Carga Horária</h5>" +
+        "<p class='card-text'>" + row.carga_horaria + "</p>" + 
+      "</div>" +
+    "</div></div>");    
+    return html.join("");
+}
+
+window.icones = 
+{
+    detailOpen: "fas fa-chevron-circle-right fa-lg",
+    detailClose: "fas fa-chevron-circle-down fa-lg"
+}
